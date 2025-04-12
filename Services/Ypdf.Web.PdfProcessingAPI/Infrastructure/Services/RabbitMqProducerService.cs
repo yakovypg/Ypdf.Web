52a1db9e2c22e8723cdb9056581f7bb6f20777e6
yakovypg <yasha-pmp@mail.ru>
2025-04-12 19:34:51 +0300

    Add logger to `RabbitMqProducerService`

--- Services/Ypdf.Web.PdfProcessingAPI/Infrastructure/Services/RabbitMqProducerService.cs
+++ Services/Ypdf.Web.PdfProcessingAPI/Infrastructure/Services/RabbitMqProducerService.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 using Ypdf.Web.Domain.Models.Configuration;
 
@@ -10,12 +11,19 @@ namespace Ypdf.Web.PdfProcessingAPI.Infrastructure.Services;
 
 public class RabbitMqProducerService : IRabbitMqProducerService
 {
+    private readonly ILogger<RabbitMqProducerService> _logger;
+
     private readonly string _hostName;
     private readonly string _queueName;
 
-    public RabbitMqProducerService(IConfiguration configuration)
+    public RabbitMqProducerService(
+        IConfiguration configuration,
+        ILogger<RabbitMqProducerService> logger)
     {
         ArgumentNullException.ThrowIfNull(configuration, nameof(configuration));
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
+
+        _logger = logger;
 
         _hostName = configuration.GetSection("RabbitMQ:HostName").Value
             ?? throw new ConfigurationException("Host name for RabbitMQ not specified");
@@ -54,6 +62,8 @@ public class RabbitMqProducerService : IRabbitMqProducerService
             .ConfigureAwait(false);
 
         byte[] body = Encoding.UTF8.GetBytes(message);
+
+        _logger.LogInformation("Send message to RabbitMQ: {Message}", message);
 
         await channel
             .BasicPublishAsync(string.Empty, _queueName, body)

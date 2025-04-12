6b5a3f5303f64a0ee5a04d0323545ff4878397c3
yakovypg <yasha-pmp@mail.ru>
2025-04-12 19:35:39 +0300

    Refactor code

--- Services/Ypdf.Web.PdfOperationsHistoryAPI/Commands/GetHistoryCommand.cs
+++ Services/Ypdf.Web.PdfOperationsHistoryAPI/Commands/GetHistoryCommand.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Threading.Tasks;
 using AutoMapper;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Ypdf.Web.Domain.Commands;
 using Ypdf.Web.Domain.Infrastructure.Extensions;
-using Ypdf.Web.Domain.Models.Configuration;
 using Ypdf.Web.PdfOperationsHistoryAPI.Models.Dto.Requests;
 using Ypdf.Web.PdfOperationsHistoryAPI.Models.Dto.Responses;
 
@@ -13,18 +11,13 @@ namespace Ypdf.Web.PdfOperationsHistoryAPI.Commands;
 
 public class GetHistoryCommand : BaseCommand, ICommand<GetHistoryRequest, GetHistoryResponse>
 {
-    private readonly IConfiguration _configuration;
-
     public GetHistoryCommand(
-        IConfiguration configuration,
         IMapper mapper,
         ILogger<BaseCommand> logger)
         : base(
             mapper ?? throw new ArgumentNullException(nameof(mapper)),
             logger ?? throw new ArgumentNullException(nameof(logger)))
     {
-        ArgumentNullException.ThrowIfNull(configuration, nameof(configuration));
-        _configuration = configuration;
     }
 
     public async Task<GetHistoryResponse> ExecuteAsync(GetHistoryRequest request)

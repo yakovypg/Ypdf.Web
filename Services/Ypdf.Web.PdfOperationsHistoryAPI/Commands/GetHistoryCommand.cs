57f403ec218dc9a290e74739215db4b85a2006a3
yakovypg <yasha-pmp@mail.ru>
2025-04-13 17:57:20 +0300

    Add the ability to get user operations history

--- Services/Ypdf.Web.PdfOperationsHistoryAPI/Commands/GetHistoryCommand.cs
+++ Services/Ypdf.Web.PdfOperationsHistoryAPI/Commands/GetHistoryCommand.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.Extensions.Logging;
 using Ypdf.Web.Domain.Commands;
+using Ypdf.Web.Domain.Models.Informing;
+using Ypdf.Web.PdfOperationsHistoryAPI.Infrastructure.Data.Repositories;
 using Ypdf.Web.PdfOperationsHistoryAPI.Models.Dto.Requests;
 using Ypdf.Web.PdfOperationsHistoryAPI.Models.Dto.Responses;
 
@@ -10,22 +14,34 @@ namespace Ypdf.Web.PdfOperationsHistoryAPI.Commands;
 
 public class GetHistoryCommand : BaseCommand, ICommand<GetHistoryRequest, GetHistoryResponse>
 {
+    private readonly IPdfOperationResultRepository _pdfOperationResultRepository;
+
     public GetHistoryCommand(
+        IPdfOperationResultRepository pdfOperationResultRepository,
         IMapper mapper,
         ILogger<BaseCommand> logger)
         : base(
             mapper ?? throw new ArgumentNullException(nameof(mapper)),
             logger ?? throw new ArgumentNullException(nameof(logger)))
     {
+        ArgumentNullException.ThrowIfNull(pdfOperationResultRepository, nameof(pdfOperationResultRepository));
+        _pdfOperationResultRepository = pdfOperationResultRepository;
     }
 
-    public async Task<GetHistoryResponse> ExecuteAsync(GetHistoryRequest request)
+    public Task<GetHistoryResponse> ExecuteAsync(GetHistoryRequest request)
     {
         ArgumentNullException.ThrowIfNull(request, nameof(request));
 
         Logger.LogInformation("Trying to get history for user {UserId}", request.UserId);
-        await Task.Delay(100).ConfigureAwait(false);
 
-        return new GetHistoryResponse();
+        IEnumerable<PdfOperationResult> results = _pdfOperationResultRepository
+            .GetAll()
+            .Where(t => t.UserId == request.UserId);
+
+        Logger.LogInformation("History for user {UserId} successfully recieved", request.UserId);
+
+        var response = new GetHistoryResponse(results);
+
+        return Task.FromResult(response);
     }
 }

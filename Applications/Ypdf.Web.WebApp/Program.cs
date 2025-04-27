7b449309127db2126e2831f7ae4b3246dbdf6704
yakovypg <yasha-pmp@mail.ru>
2025-04-28 00:06:39 +0300

    Add packages for working with Bootstrap and FontAwesome

--- Applications/Ypdf.Web.WebApp/Program.cs
+++ Applications/Ypdf.Web.WebApp/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +11,16 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddRazorComponents()
     .AddInteractiveServerComponents();
+
+_ = builder.Services.AddServerSideBlazor();
+
+_ = builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
 
 WebApplication webApplication = builder.Build();
 

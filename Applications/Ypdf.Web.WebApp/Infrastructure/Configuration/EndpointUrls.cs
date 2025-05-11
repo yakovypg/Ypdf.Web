31242c95460c59639b976d40179e983e755b9fbe
yakovypg <yasha-pmp@mail.ru>
2025-05-11 17:34:19 +0300

    Update waiting page

--- Applications/Ypdf.Web.WebApp/Infrastructure/Configuration/EndpointUrls.cs
+++ Applications/Ypdf.Web.WebApp/Infrastructure/Configuration/EndpointUrls.cs
@@ -10,10 +10,25 @@ public static class EndpointUrls
 {
     public static Uri Login => new("https://localhost:8081/api/account/login/");
     public static Uri Register => new("https://localhost:8081/api/account/register/");
-    public static Uri HistoryBase => new("https://localhost:8083/api/history/");
 
     public static Uri Merge => new("https://localhost:8082/api/tool/merge/");
     public static Uri Split => new("https://localhost:8082/api/tool/split/");
+
+    public static Uri OutputFileBase => new("https://localhost:8082/api/output/");
+    public static Uri CheckOutputFileBase => new("https://localhost:8082/api/output/check/");
+    public static Uri HistoryBase => new("https://localhost:8083/api/history/");
+
+    public static Uri OutputFile(string fileName)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(fileName, nameof(fileName));
+        return new Uri(OutputFileBase, fileName);
+    }
+
+    public static Uri CheckOutputFile(string fileName)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(fileName, nameof(fileName));
+        return new Uri(CheckOutputFileBase, fileName);
+    }
 
     public static Uri History(string userId, int pageNumber, int pageSize)
     {

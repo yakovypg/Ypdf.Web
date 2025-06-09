da2981c6f6feaea6168bdaa14fb58d7bf4c3cde2
yakovypg <yasha-pmp@mail.ru>
2025-06-09 19:41:01 +0300

    Update checking script

--- Deploy/Production/Scripts/check.sh
+++ Deploy/Production/Scripts/check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-kubectl get apiservices
+kubectl get apiservices | grep metrics
 kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/ypdf/rabbitmq_queue_messages"
 kubectl get hpa pdf-processing-api-hpa -n ypdf
 
-kubectl -n monitoring port-forward svc/kube-prom-stack-kube-prome-prometheus 9090:9090
-minikube tunnel
+# kubectl -n monitoring port-forward svc/kube-prom-stack-kube-prome-prometheus 9090:9090
+# minikube tunnel

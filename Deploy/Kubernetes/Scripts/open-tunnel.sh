feedc188531adbc28ea8419af5d8997000fa5592
yakovypg <yasha-pmp@mail.ru>
2025-06-20 17:58:45 +0300

    Update open-tunnel and tunnel-prometheus scripts

--- Deploy/Kubernetes/Scripts/open-tunnel.sh
+++ Deploy/Kubernetes/Scripts/open-tunnel.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 set -euo pipefail
+
+echo Ypdf web application will be available at https://localhost:9001
 minikube tunnel
--- Deploy/Kubernetes/Scripts/tunnel-prometheus.sh
+++ Deploy/Kubernetes/Scripts/tunnel-prometheus.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 set -euo pipefail
+
+echo Prometheus will be available at http://localhost:9090
 kubectl -n monitoring port-forward svc/kube-prom-stack-kube-prome-prometheus 9090:9090

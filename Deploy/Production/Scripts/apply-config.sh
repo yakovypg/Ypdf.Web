b0352f7a9021909a9acae66e5007cbf55a471209
yakovypg <yasha-pmp@mail.ru>
2025-06-20 15:06:07 +0300

    Rename configurations

--- Deploy/Production/Scripts/apply-config.sh
+++ Deploy/Production/Scripts/apply-config.sh
@@ -1,55 +1,62 @@
 #!/bin/bash
 
-apply_all_resources() {
-    kubectl apply -f ../namespace.yml
+set -euo pipefail
 
-    kubectl apply -f ../tls-secret.yml
+kubectl apply -f ../namespace.yml
 
-    kubectl apply -f ../accounts-database-pvc.yml
-    kubectl apply -f ../db-data-pvc.yml
-    kubectl apply -f ../intput-files-pvc.yml
-    kubectl apply -f ../output-files-pvc.yml
-    kubectl apply -f ../web-app-keys-pvc.yml
+kubectl apply -f ../pvc-accounts-database.yml
+kubectl apply -f ../pvc-db-data.yml
+kubectl apply -f ../pvc-intput-files.yml
+kubectl apply -f ../pvc-output-file.yml
+kubectl apply -f ../pvc-web-app-keys.yml
 
-    kubectl apply -f ../accounts-database-deployment.yml
-    kubectl apply -f ../accounts-database-service.yml
-    kubectl apply -f ../rabbitmq-deployment.yml
-    kubectl apply -f ../rabbitmq-service.yml
+kubectl apply -f ../secret-tls.yml
 
-    kubectl apply -f ../account-api-deployment.yml
-    kubectl apply -f ../files-api-deployment.yml
-    kubectl apply -f ../pdf-processing-api-deployment.yml
-    kubectl apply -f ../pdf-operations-history-api-deployment.yml
-    kubectl apply -f ../web-app-deployment.yml
+kubectl apply -f ../deployment-accounts-database.yml
+kubectl apply -f ../deployment-rabbitmq.yml
+kubectl apply -f ../deployment-rabbitmq-exporter.yml
 
-    kubectl apply -f ../account-api-service.yml
-    kubectl apply -f ../files-api-service.yml
-    kubectl apply -f ../pdf-processing-api-service.yml
-    kubectl apply -f ../pdf-operations-history-api-service.yml
-    kubectl apply -f ../web-app-service.yml
+kubectl apply -f ../service-accounts-database.yml
+kubectl apply -f ../service-rabbitmq.yml
+kubectl apply -f ../service-rabbitmq-exporter.yml
 
-    kubectl apply -f ../rabbitmq-exporter-deployment.yml
+kubectl apply -f ../deployment-account-api.yml
+kubectl apply -f ../deployment-files-api.yml
+kubectl apply -f ../deployment-pdf-processing-api.yml
+kubectl apply -f ../deployment-pdf-operations-history-api.yml
+kubectl apply -f ../deployment-web-app.yml
 
-    helm upgrade --install metrics-server metrics-server/metrics-server --namespace kube-system --create-namespace --set args[0]=--kubelet-insecure-tls --set args[1]="--kubelet-preferred-address-types=InternalIP\,Hostname\,ExternalIP"
-    helm upgrade --install kube-prom-stack prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace --set grafana.enabled=true --set prometheusOperator.createCustomResource=true --set prometheusOperator.admissionWebhooks.enabled=false --set prometheusOperator.admissionWebhooks.patch.enabled=false --set prometheusOperator.tls.enabled=false --set prometheus.prometheusSpec.serviceMonitorSelector.matchLabels.release=kube-prom-stack --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false -f ../values.yml
-    kubectl apply -f ../rabbitmq-exporter-servicemonitor.yml
-    helm upgrade --install prometheus-adapter prometheus-community/prometheus-adapter --namespace monitoring -f ../adapter-values.yaml
+kubectl apply -f ../service-account-api.yml
+kubectl apply -f ../service-files-api.yml
+kubectl apply -f ../service-pdf-processing-api.yml
+kubectl apply -f ../service-pdf-operations-history-api.yml
+kubectl apply -f ../service-web-app.yml
 
-    kubectl apply -f ../pdf-processing-api-hpa.yml
+helm upgrade \
+    --install metrics-server metrics-server/metrics-server \
+    --namespace kube-system \
+    --create-namespace \
+    --set args[0]=--kubelet-insecure-tls \
+    --set args[1]="--kubelet-preferred-address-types=InternalIP\,Hostname\,ExternalIP"
 
-    kubectl run curlpod -n ypdf --image=curlimages/curl --restart=Never --command -- sleep 3600
-    kubectl run curlpod -n monitoring --image=curlimages/curl --restart=Never --command -- sleep 3600
-}
+helm upgrade \
+    --install kube-prom-stack prometheus-community/kube-prometheus-stack \
+    --namespace monitoring \
+    --create-namespace \
+    --set grafana.enabled=true \
+    --set prometheusOperator.createCustomResource=true \
+    --set prometheusOperator.admissionWebhooks.enabled=false \
+    --set prometheusOperator.admissionWebhooks.patch.enabled=false \
+    --set prometheusOperator.tls.enabled=false \
+    --set prometheus.prometheusSpec.serviceMonitorSelector.matchLabels.release=kube-prom-stack \
+    --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false \
+    -f ../values.yml
 
-name=$1
-all_configs="all"
+kubectl apply -f ../servicemonitor-rabbitmq-exporter.yml
 
-if [[ -z $name ]]; then
-    name=$all_configs
-fi
+helm upgrade \
+    --install prometheus-adapter prometheus-community/prometheus-adapter \
+    --namespace monitoring \
+    -f ../adapter-values.yaml
 
-if [[ $name == $all_configs ]]; then
-    apply_all_resources
-else
-    kubectl apply -f "../${name}"
-fi
+kubectl apply -f ../hpa-pdf-processing-api.yml
--- Deploy/Production/Scripts/check.sh
+++ Deploy/Production/Scripts/check.sh
@@ -2,7 +2,7 @@
 
 kubectl get apiservices | grep metrics
 kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/ypdf/rabbitmq_queue_messages"
-kubectl get hpa pdf-processing-api-hpa -n ypdf
+kubectl get hpa hpa-pdf-processing-api -n ypdf
 
 # kubectl -n monitoring port-forward svc/kube-prom-stack-kube-prome-prometheus 9090:9090
 # minikube tunnel

0f6ed0227c601eb9acc74f0e74e26b0e5dfea516
yakovypg <yasha-pmp@mail.ru>
2025-06-21 17:11:23 +0300

    Update `delete-all.sh` script

--- Deploy/Kubernetes/Scripts/delete-all.sh
+++ Deploy/Kubernetes/Scripts/delete-all.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 
 kubectl delete all --all -n monitoring
 kubectl delete all --all -n ypdf
+kubectl delete servicemonitor --all -n ypdf
 kubectl delete pvc --all -n ypdf
+kubectl delete secrets --all -n ypdf
